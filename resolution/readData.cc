c6e5ee5e69f4a40b84b46b5ac8e50c6dbd28b92e
Wenzer Qin <wenzer.qin@gmail.com>
2016-06-21 17:32:57 +0200

    added options to exclude types of jets in resolution

--- resolution/readData.cc
+++ resolution/readData.cc
@@ -15,8 +15,8 @@ void readData(char* channel="4e")
     else if(strcmp(channel,"2l2q")==0){
       width[i] = 3.21246+0.0312538*(massBin[i])-7.29127e-07*(massBin[i])*(massBin[i]);
     }
-    xMin[i] = width[i]*(-30);
-    xMax[i] = width[i]*(25);
+    xMin[i] = width[i]*(-15);
+    xMax[i] = width[i]*(10);
   }
 
   if (ZZCandType == 1)
@@ -28,7 +28,7 @@ void readData(char* channel="4e")
   dataset = new RooDataSet("resoM","resoM",ntupleVarSet,WeightVar("myW"));
 
   for (int i=0; i<Nfiles; i++) {
-     sprintf(inputfile,"wqin_test2/mytree/PT13TeV/ggH_zz2l2q_M%d/ZZ2l2qAnalysis.root",inputfiles[i]);
+     sprintf(inputfile,"2016_2l2q/mytree/PT13TeV/ggH_zz2l2q_M%d/ZZ2l2qAnalysis.root",inputfiles[i]);
     files.push_back(inputfile);
   }
 
@@ -57,32 +57,60 @@ void readData(char* channel="4e")
 
     for(int k=0; k<nentries; k++){
       candTree->GetEvent(k);
-//      if (weight <= 0 ) cout << "Warning! Negative weight events" << endl;
       if (PUWeight*genHEPMCweight <= 0 ) cout << "Warning! Negative weight events" << endl;
 
-      for (int w=0; w < (*candType).size(); w++) {
-       if((strcmp(channel,"4mu")==0) && (z1flav->at(w))*(z2flav->at(w)) != 28561) continue;
-       if((strcmp(channel,"4e")==0) && (z1flav->at(w))*(z2flav->at(w)) != 14641) continue;
-       if((strcmp(channel,"2e2mu")==0) && (z1flav->at(w))*(z2flav->at(w)) != 20449) continue;
+      switch (exclude){
+      case 0:
+        for (int w=0; w < (*candType).size(); w++) {
+         if (candType->at(w)==ZZCandType) {
+           for (int i=0; i<maxMassBin; i++) {
+             ntupleVarSet.setCatIndex("massrc",massBin[i]);
+             ntupleVarSet.setRealValue("reso",(m4l->at(w))-genM);
+             ntupleVarSet.setRealValue("myW",PUWeight*genHEPMCweight);
 
-       if (candType->at(w)==ZZCandType) {
+             if((ZZCandType==1) && ((zzsel->at(w))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))))
+               dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
+             else if((ZZCandType==2) && (((Z1Mass->at(w))>70)&&((Z1Mass->at(w))<105)) && ((zzsel->at(w))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))) && ((massBin[i]<1200)||((massBin[i]>=1200)&&(((m4l->at(w))-genM)>-500))))
+               dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
+           }
+         }
+        }
+        break;
+      case 1:
+        for (int w=0; w < (*candType).size(); w++) {
+         if ((candType->at(w)==ZZCandType) && ((*candType).size()==2)) {
+           for (int i=0; i<maxMassBin; i++) {
+             ntupleVarSet.setCatIndex("massrc",massBin[i]);
+             ntupleVarSet.setRealValue("reso",(m4l->at(w))-genM);
+             ntupleVarSet.setRealValue("myW",PUWeight*genHEPMCweight);
+
+             if((ZZCandType==1) && ((zzsel->at(w))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))))
+               dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
+             else if((ZZCandType==2) && (((Z1Mass->at(w))>70)&&((Z1Mass->at(w))<105)) && ((zzsel->at(w))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))) && ((massBin[i]<1200)||((massBin[i]>=1200)&&(((m4l->at(w))-genM)>-500))))
+               dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
+           }
+         }
+        }
+        break;
+      case 2:
+       if ((candType->at(0)==ZZCandType) && ((*candType).size()==1)) {
          for (int i=0; i<maxMassBin; i++) {
            ntupleVarSet.setCatIndex("massrc",massBin[i]);
-           ntupleVarSet.setRealValue("reso",(m4l->at(w))-genM);
-           ntupleVarSet.setRealValue("myW",PUWeight*genHEPMCweight);	
-//           ntupleVarSet.setRealValue("myW",weight*(wt->at(7)));
-//           if((((Z1Mass->at(w))>70)&&((Z1Mass->at(w))<105)) && ((zzsel->at(w))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))))
-           if(((zzsel->at(w))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))))
-//for(auto && zz : zzsel)
-//      if((zz>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))))
-//             if ((massBin[i]!=2000)||((massBin[i]==2000)&&(((m4l->at(w))-genM)>-500)))
-               dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
-//             dataset.add(ntupleVarSet, weight*(wt->at(7)));
+           ntupleVarSet.setRealValue("reso",(m4l->at(0))-genM);
+           ntupleVarSet.setRealValue("myW",PUWeight*genHEPMCweight);
 
+           if((ZZCandType==1) && ((zzsel->at(0))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))))
+             dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
+           else if((ZZCandType==2) && (((Z1Mass->at(0))>70)&&((Z1Mass->at(0))<105)) && ((zzsel->at(0))>=100 && x.getVal()>xMin[i] && x.getVal()<xMax[i])&&((massBin[i]<1000&&genM>(massBin[i]-5)&&genM<(massBin[i]+5))||(massBin[i]>=1000&&genM>(massBin[i]*0.95)&&genM<(massBin[i]*1.05))) && ((massBin[i]<1200)||((massBin[i]>=1200)&&(((m4l->at(0))-genM)>-500))))
+             dataset->add(ntupleVarSet, PUWeight*genHEPMCweight);
+          }
         }
+        break;
+      default:
+        cout << endl << "Bad value for \"exclude\"." << endl;
+        break;
       }
-    }
-  }
+    } 
 
   cout << "dataset n entries: " << dataset->sumEntries() << endl;
 
